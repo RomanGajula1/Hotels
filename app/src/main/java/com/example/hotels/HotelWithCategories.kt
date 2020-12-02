6f7f2ac342cab5e43a62f8fa50e28fd3bba2b9d6
roman_gajula <rgajula@bk.ru>
2020-12-02 11:32:27 +0200

    Hotels

--- app/src/main/java/com/example/hotels/HotelWithCategories.kt
+++ app/src/main/java/com/example/hotels/HotelWithCategories.kt
@@ -1,44 +1,32 @@
-package com.example.hotels.VIEW
+package com.example.hotels
 
-import android.annotation.SuppressLint
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.hotels.HotelWithCategories
-import com.example.hotels.R
+import com.example.hotels.VIEW.MainActivity
+import com.example.hotels.databinding.ActivityHotelWithCategoriesBinding
 import com.example.hotels.viewModel.ListHotelViewModel
-import com.example.hotels.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : AppCompatActivity() {
+class HotelWithCategories : AppCompatActivity() {
 
     private val hotelsListViewModel: ListHotelViewModel by viewModel()
 
-    @SuppressLint("WrongConstant")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding =
-                DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val binding: ActivityHotelWithCategoriesBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_hotel_with_categories)
 
         binding.apply {
             recyclerView.setHasFixedSize(true)
-            recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
-            recyclerView.adapter = this@MainActivity.hotelsListViewModel.adapter
+            recyclerView.layoutManager = LinearLayoutManager(this@HotelWithCategories)
+            recyclerView.adapter = this@HotelWithCategories.hotelsListViewModel.adapter
         }
         hotelsListViewModel.adapter.setData(hotelsListViewModel.hotelList)
-    }
-    fun clickAddHotel(view: View){
-        val intent = Intent(this@MainActivity, AddHotel::class.java)
-        startActivity(intent)
     }
     @Override
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
