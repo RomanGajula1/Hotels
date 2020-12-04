531f91525e2084cc87cd11075a0d81a9e058c34a
roman_gajula <rgajula@bk.ru>
2020-12-04 11:21:51 +0200

    Hotels

--- app/src/main/java/com/example/hotels/model/HotelCategories.kt
+++ app/src/main/java/com/example/hotels/model/HotelCategories.kt
@@ -1,6 +1,8 @@
 package com.example.hotels.model
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.CASCADE
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "hotelCategories")
